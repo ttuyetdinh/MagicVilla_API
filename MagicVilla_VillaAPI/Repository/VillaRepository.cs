5c9a5df06cb4fd02075710e10650b01cc84ba318
Phuc Nguyen <phuc.nguyen@blackbook.ai>
2023-12-27 15:47:34 +0700

    refactor repository

--- MagicVilla_VillaAPI/Repository/VillaRepository.cs
+++ MagicVilla_VillaAPI/Repository/VillaRepository.cs
@@ -11,58 +11,23 @@ using Microsoft.OpenApi.Expressions;
 
 namespace MagicVilla_VillaAPI.Repository
 {
-    public class VillaRepository : IVillaRepository
+    public class VillaRepository : Repository<Villa>, IVillaRepository
     {
         private readonly ApplicationDbContext _db;
-        public VillaRepository(ApplicationDbContext db)
+        public VillaRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
         }
-        public async Task CreateAsync(Villa entity)
-        {
-            await _db.AddAsync(entity);
-            await SaveAsync();
-        }
-
-        public async Task<Villa> GetAsync(Expression<Func<Villa, bool>> filter = null, bool tracked = true)
-        {
-            IQueryable<Villa> query = _db.Villas;
-
-            query = !tracked ? query.AsNoTracking() : query;
-
-            query = filter != null ? query.Where(filter) : query;
-
-            return await query.FirstOrDefaultAsync();
-        }
-
-        public async Task<List<Villa>> GetAllAsync(Expression<Func<Villa, bool>> filter = null)
-        {
-            IQueryable<Villa> query = _db.Villas;
-
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-
-            return await query.ToListAsync();
-
-        }
-
-        public async Task RemoveAsync(Villa entity)
-        {
-            _db.Villas.Remove(entity);
-            await SaveAsync();
-        }
-
-        public async Task UpdateAsync(Villa entity)
+        public async Task<Villa> UpdateAsync(Villa entity)
         {
             _db.Villas.Update(entity);
+            
+            // can use the method inherit from the Repository<Villa>
+            // because they are reference to the same dbcontext instance
+            // await _db.SaveChangesAsync();
             await SaveAsync();
-        }
 
-        public async Task SaveAsync()
-        {
-            await _db.SaveChangesAsync();
+            return entity;
         }
     }
 }
